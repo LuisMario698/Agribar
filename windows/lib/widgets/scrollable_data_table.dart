e90de8495a47a2f586aeff20ab779fa2a8710d79
Luis Mario Suarez Gutierrez <mario@MacBook-Air-de-Luis.local>
2025-11-11 19:16:11 -0700

    Avance?

--- windows/lib/widgets/scrollable_data_table.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// This widget is deprecated. Please use the local table implementation in Nomina_screen.dart
-class ScrollableDataTable extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError('Use the embedded NominaScreen table implementation');
-  }
-}
